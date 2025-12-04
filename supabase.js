4bb101130c4727773378dbac800f64b32eb42682
vinh-dotcom <nguyenquangvinh126@gmail.com>
2025-12-04 16:15:17 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,23 @@
-// Thay SUPABASE_URL và SUPABASE_ANON_KEY bằng thông tin của project Supabase
+// supabase.js
 const SUPABASE_URL = "https://nhzkktsnsaclwwchzdkc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oemtrdHNuc2FjbHd3Y2h6ZGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODMyMjUsImV4cCI6MjA4MDE1OTIyNX0.Z9MhK_AaJhapRhn0m8RDDURrcEKmRSFqVF_XMJROBWg";
 
-const { createClient } = supabase;  // supabase-js đã load từ CDN
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  console.error("Supabase keys missing");
+}
+
+if (typeof Supabase === 'undefined') {
+  console.error("Supabase library not loaded! Check CDN script in index.html.");
+} else {
+  console.log("Supabase library loaded successfully.");
+}
+
+window.supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    storage: window.localStorage,   // QUAN TRỌNG NHẤT
+    autoRefreshToken: true
+  }
+});
+
+console.log("Supabase client initialized.");
