db42d766122f1a3e2f1b2e317f3a9dae4b53b864
vinh-dotcom <nguyenquangvinh126@gmail.com>
2025-12-04 15:50:15 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,14 +2,5 @@
 const SUPABASE_URL = "https://nhzkktsnsaclwwchzdkc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oemtrdHNuc2FjbHd3Y2h6ZGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODMyMjUsImV4cCI6MjA4MDE1OTIyNX0.Z9MhK_AaJhapRhn0m8RDDURrcEKmRSFqVF_XMJROBWg";
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  console.error("Supabase keys missing");
-}
-
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    storage: window.localStorage,   // 🚀 QUAN TRỌNG NHẤT
-    autoRefreshToken: true
-  }
-});
+const { createClient } = supabase;  // supabase-js đã load từ CDN
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
